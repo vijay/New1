2792e9c5c33497bce4dcfa98c5449a3d93112145
Vijay <vijaypakala@gmail.com>
2017-02-25 20:37:32 +0530

    commit

---
+++ Sampl1/src/pack1/page1.java
@@ -0,0 +1,8 @@
+package pack1;
+
+public class page1 {
+
+	public void m1(){
+		System.out.println("Hello how are you");
+	}
+}
---
+++ Sampl1/src/test1/test1.java
@@ -0,0 +1,11 @@
+package test1;
+
+import pack1.page1;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		page1 p=new page1();
+		p.m1();
+	}
+}
